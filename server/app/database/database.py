8594c412c18e9eef65389228aa71d26b6c457596
Tomás Sarmiento <t.sarmiento01@ufromail.cl>
2024-04-17 14:59:17 -0400

    feat: add .env backend and add new requirements

--- server/app/database/database.py
+++ server/app/database/database.py
@@ -2,9 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
+from environs import Env
 
+env = Env()
+env.read_env()
 
-DATABASE_URL = "postgresql://postgres:password@localhost/bloomalert"
+DATABASE_URL = env.str("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
