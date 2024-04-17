04896c602ce530c2caad3805795c4b371f86d67f
Tomás Sarmiento <t.sarmiento01@ufromail.cl>
2024-04-17 13:43:08 -0400

    feat: add ḧeadings

--- client/src/components/OrganizationSelector.tsx
+++ client/src/components/OrganizationSelector.tsx
@@ -22,7 +22,6 @@ const OrganizationSelector: React.FC = () => {
  const handleOrganizationChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const orgId = event.target.value;
     setSelectedOrg(orgId);
-
     fetch(`http://localhost:8000/timeseries/${orgId}`)
       .then(response => response.json())
       .then(data => setTimeseriesData(data));
@@ -34,22 +33,24 @@ const OrganizationSelector: React.FC = () => {
  };
 
  return (
-    <div className="container mx-auto p-10">
+    <div className="flex flex-col justify-center items-center gap-5 container mx-auto p-10">
+      <h1 className="text-2xl">Select an organization to see it's related data</h1>
       <select
         className="mb-10 block mx-auto appearance-none w-[20vw] bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
         value={selectedOrg}
         onChange={handleOrganizationChange}
       >
-        <option value="">Select an organization</option>
+        <option value="default">Select an organization</option>
         {organizations.map(org => (
           <option key={org.organization} value={org.organization}>{org.organization}</option>
         ))}
       </select>
       {(selectedOrg) && (
-        <div className="flex flex-col justify-center items-center gap-10">
-          <MapContainer center={[0,0]} zoom={13} style={{ height: "70vh", width: "70%", marginBottom: "2rem" }}>
+        <div className="mx-auto flex flex-col justify-center items-center gap-10 w-[60vw]">
+          <MapContainer center={[0,0]} zoom={13} style={{ height: "70vh", width: "100%", marginBottom: "2rem" }}>
             <Map polygon={selectedOrgPolygon} />
           </MapContainer>
+          <h1 className="text-2xl">Timeseries data for {selectedOrg}</h1>
           <TimeSeriesChart timeseriesData={timeseriesData} />
           <TimeseriesTable timeseriesData={timeseriesData}/>
         </div>
